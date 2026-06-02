f7c86f8f0900dd02c172ab307ffec3e8e4247153
Felipe Monteiro <felipemonteiro.silva@outlook.com>
2026-06-02 09:00:06 -0400

    Preparation for Task 6

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+from EmotionDetection.emotion_detector import emotion_detector
+
+app = Flask("EmotionDetection")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/emotionDetector")
+def detect_emotion():
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    emotion = emotion_detector(text_to_analyze)
+
+    return jsonify(emotion)
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000)
+
